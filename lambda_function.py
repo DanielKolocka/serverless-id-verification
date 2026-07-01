00a506dba9151894674f2d6d6a131fa00d94f56b
Daniel Kolocka <kolockadaniel@hotmail.com>
2026-07-01 13:36:28 -0700

    add get and post method to lambda handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
+import json
 import re
 import uuid
 import boto3
 from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+
 
 
 dynamodb = boto3.resource('dynamodb')
@@ -22,9 +25,21 @@ def validate_id(id_number: str, country_code: str) -> dict:
     }
 
 def lambda_handler(event, context):
-    try:   
+    try: 
+        if event.get('requestContext', {}).get('http', {}).get('method') == 'GET':
+            request_id = event.get('pathParameters', {}).get('requestID')
+            if not request_id:
+                return {'statusCode': 400, 'body': json.dumps({'error': 'requestId required'})}
+    
+            response = myTable.get_item(Key={'requestID': request_id})
+            item = response.get('Item')
+    
+            if not item:
+                return {'statusCode': 404, 'body': json.dumps({'error': 'Not found'})}
+    
+            return {'statusCode': 200, 'body': json.dumps(item, default=str)}  
         print('Event:', event)
-        body = event.get('body', {})
+        body = json.loads(event.get('body', {}))
         id_number = body.get('id_number')
         country_code = body.get('country_code')
 
@@ -33,19 +48,20 @@ def lambda_handler(event, context):
         if not id_number or not country_code:
             return {
                 'statusCode': 400,
-                'body': {
+                'body': json.dumps({
                     'message': 'id_number and country_code are required.'
-                }
+                })
             }
         
         result = validate_id(id_number, country_code)
 
         # Build the record to store in DB
-        request_id = str(uuid.uuid4())
-        timestamp = datetime.now(timezone.pst).isoformat()
+        requestID = str(uuid.uuid4())
+        print(f'Generated request_id: {requestID}')
+        timestamp = datetime.now(ZoneInfo("America/Los_Angeles")).isoformat()
 
         record = {
-            'request_id': request_id,
+            'requestID': requestID,
             'id_number': id_number,
             'country_code': country_code,
             'is_valid': result['is_valid'],
@@ -55,20 +71,32 @@ def lambda_handler(event, context):
 
         myTable.put_item(Item=record)
 
-        return {
-            'statusCode': 200,
-            'body': {
-                'request_id': request_id,
+        if not record['is_valid']:
+            return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'requestID': requestID,
                 'is_valid': result['is_valid'],
                 'errors': result['errors'],
                 'timestamp': timestamp
-            }
+            })
+        }
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'requestID': requestID,
+                'is_valid': result['is_valid'],
+                'errors': result['errors'],
+                'timestamp': timestamp
+            })
         }
     except Exception as e:
         print(f'Error: {str(e)}')
         return {
             'statusCode': 500,
-            'body': {
+            'body': json.dumps({
                 'error': str(e)
-            }
+            })
         }
+
